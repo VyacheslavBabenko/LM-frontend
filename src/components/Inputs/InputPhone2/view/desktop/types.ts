14bd6c70f2e024991370d847d7808c078ab0f8ed
Vyacheslav <mr.aivengo@gmail.com>
2024-10-27 20:05:31 +0200

    merge

--- src/components/Inputs/InputPhone2/view/desktop/types.ts
+++ src/components/Inputs/InputPhone2/view/desktop/types.ts
@@ -1,10 +1,6 @@
 import { PhoneInputProps } from 'react-phone-input-2';
 
 interface IInputPhone2Props extends PhoneInputProps {
-  color?: 'default';
-  fontSize?: 16;
-  measure?: string;
-  isPassword?: boolean;
   error?: string | null;
   isValid?: boolean;
   setIsValid?: (value: boolean) => void;
